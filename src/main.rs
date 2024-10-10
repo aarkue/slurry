a5ffe4acb637c6ff3743ddf12081aa063bd36a3e
aarkue <git@aarkue.eu>
2024-10-10 11:24:22 +0200

    More experiments (#of new jobs)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use std::{collections::HashMap, time::SystemTime};
+use std::{collections::{HashMap, HashSet}, time::{Duration, SystemTime}};
 
 use anyhow::Error;
 use async_ssh2_tokio::client::{AuthMethod, Client, ServerCheckMethod};
 use chrono::{DateTime, Utc};
+use tokio::time::sleep;
 
 // https://slurm.schedmd.com/squeue.html
-const SQUEUE_FORMAT_STR: &str = "%a|%A|%B|%c|%C|%D|%e|%E|%f|%F|%g|%G|%i|%l|%L|%j|%m|%M|%p|%P|%T|%r|%S|%V|%Z|%o";
+const SQUEUE_FORMAT_STR: &str =
+    "%a|%A|%B|%c|%C|%D|%e|%E|%f|%F|%g|%G|%i|%l|%L|%j|%m|%M|%p|%P|%T|%r|%S|%V|%Z|%o";
 const SQUEUE_EXPECTED_COLS: &[&str] = &[
     "ACCOUNT",
     "JOBID",
@@ -44,24 +46,37 @@ async fn main() -> Result<(), Error> {
         ServerCheckMethod::DefaultKnownHostsFile,
     )
     .await?;
+    let (time,d) = get_squeue_res(&client).await?;
+    println!("{:#?}", d[0]);
+    println!("Got {} results", d.len());
+    println!("Time of Querying: {}", time.to_rfc3339());
 
+    let job_ids: HashSet<String> = d.iter().map(|c| c.get("JOBID").cloned().unwrap_or_default()).collect();
+    println!("Got {} jobs",job_ids.len());
+    sleep(Duration::from_secs(20)).await;
+
+
+    let (time,d) = get_squeue_res(&client).await?;
+    let job_ids_2: HashSet<String> = d.iter().map(|c| c.get("JOBID").cloned().unwrap_or_default()).collect();
+    println!("Got {} NEW jobs",job_ids_2.difference(&job_ids).count());
+
+    Ok(())
+}
+
+async fn get_squeue_res<'a>(client: &'a Client) -> Result<(DateTime<Utc>, Vec<HashMap<String,String>>), Error> {
     let result = client
         .execute(&format!("squeue --format='{SQUEUE_FORMAT_STR}'"))
         .await?;
-    let mut res_lines = result
-    .stdout
-    .split("\n");
-    let column_str = 
-        res_lines.next()
+    let mut res_lines = result.stdout.split("\n");
+    let column_str = res_lines
+        .next()
         .ok_or(Error::msg("No line breaks in output"))?
         .to_string();
     let columns: Vec<&str> = column_str.split("|").collect();
-    println!("{:?}", columns);
-    assert_eq!(columns,SQUEUE_EXPECTED_COLS);
+    assert_eq!(columns, SQUEUE_EXPECTED_COLS);
     let time: DateTime<Utc> = SystemTime::now().into();
-    let d: Vec<HashMap<_,_>> = res_lines.map(|line| columns.iter().zip(line.split("|")).collect()).collect();
-    println!("{:#?}",d[0]);
-    println!("Got {} results",d.len());
-    println!("Time of Querying: {}",time.to_rfc3339());
-    Ok(())
+    let d: Vec<HashMap<_, _>> = res_lines
+        .map(move |line| columns.iter().zip(line.split("|")).map(|(col_name,val)| (col_name.to_string(),val.to_string())).collect())
+        .collect();
+    Ok((time, d))
 }
