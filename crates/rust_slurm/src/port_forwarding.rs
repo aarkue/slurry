9df5c55a53fbaab44d0ac48fadc5e22d6834680c
aarkue <git@aarkue.eu>
2025-01-21 17:10:38 +0100

    Return join handle when port forwarding

--- crates/rust_slurm/src/port_forwarding.rs
+++ crates/rust_slurm/src/port_forwarding.rs
@@ -4,13 +4,13 @@ use std::{
 };
 
 use anyhow::Error;
-use tokio::net::TcpListener;
+use tokio::{net::TcpListener, task::{self, JoinHandle}};
 
 pub async fn ssh_port_forwarding<S: AsRef<str>>(
     client: Arc<async_ssh2_tokio::Client>,
     local_addr: S,
     remote_addr: S,
-) -> Result<(),Error>{
+) -> Result<JoinHandle<()>,Error>{
     println!("Got client!");
     let l_addr: SocketAddr = local_addr.as_ref().parse().unwrap();
     let local_listener = TcpListener::bind(l_addr)
@@ -18,40 +18,42 @@ pub async fn ssh_port_forwarding<S: AsRef<str>>(
         .expect("Cannot bind local port");
     let arc = std::sync::Arc::new(client);
     let r_addr: SocketAddr = remote_addr.as_ref().parse().unwrap();
-    loop {
-        let (mut socket, _) = local_listener
+    let f = task::spawn(async move {
+        loop {
+            let (mut socket, _) = local_listener
             .accept()
             .await
             .expect("Cannot process local client");
-
+        
         println!("Client connected");
         let a = arc.clone();
         tokio::spawn(async move {
             let c = a
-                .open_direct_tcpip_channel(
-                    r_addr,
-                    SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 0),
-                )
-                .await;
-            match c {
-                Ok(channel) => {
-                    let mut ssh_stream = channel.into_stream();
-
-                    match tokio::io::copy_bidirectional(&mut socket, &mut ssh_stream).await {
-                        Ok((bytes_to_remote, bytes_to_local)) => {
-                            println!(
+            .open_direct_tcpip_channel(
+                r_addr,
+                SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 0),
+            )
+            .await;
+        match c {
+            Ok(channel) => {
+                let mut ssh_stream = channel.into_stream();
+                
+                match tokio::io::copy_bidirectional(&mut socket, &mut ssh_stream).await {
+                    Ok((bytes_to_remote, bytes_to_local)) => {
+                        println!(
                             "Connection closed. Sent {} bytes to remote, received {} bytes from remote",
                             bytes_to_remote, bytes_to_local
                         );
-                        }
-                        Err(e) => eprintln!("Error forwarding traffic: {:?}", e),
                     }
+                    Err(e) => eprintln!("Error forwarding traffic: {:?}", e),
                 }
-                Err(e) => eprintln!("Could not open channel: {:?}", e),
             }
-        });
-    }
-    Ok(())
+            Err(e) => eprintln!("Could not open channel: {:?}", e),
+        }
+    });
+}
+});
+Ok(f)
 }
 
 #[cfg(test)]
