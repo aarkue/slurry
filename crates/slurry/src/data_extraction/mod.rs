8c8a63b6289ccdf7f0be371ce88d8964031492fb
aarkue <git@aarkue.eu>
2025-07-07 17:34:50 +0200

    Release v0.1.4

--- crates/slurry/src/data_extraction/mod.rs
+++ crates/slurry/src/data_extraction/mod.rs
@@ -2,4 +2,7 @@
 /// Module for extracting data using the `squeue` command
 pub mod squeue;
 
-pub use squeue::{get_squeue_res, get_squeue_res_locally, get_squeue_res_ssh, squeue_diff, SqueueMode};
+pub use squeue::{get_squeue_res, get_squeue_res_locally, squeue_diff, SqueueMode};
+
+#[cfg(feature = "ssh")]
+pub use squeue::{get_squeue_res_ssh};
